ddaa2fc2ca13f69acc77a37bacd38fdde2f89208
georgerangelcode <georgerangelcode@gmail.com>
2019-06-25 17:30:43 -0500

    Integración a Platzi Music

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{msg}}
+    section.section
+
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
+          a.button.is-info.is-large(v-on:click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{searchMessage}}
+
+      .container.results
+        .columns
+          .column(v-for="track in tracks") {{track.nombre}} - {{track.artist}}
 </template>
 
 <script>
+const tracks = [
+  { nombre: "cancion 1", artist: "artist 1" },
+  { nombre: "cancion 2", artist: "artist 2" },
+  { nombre: "cancion 3", artist: "artist 3" }
+];
+
 export default {
   name: "app",
+
   data() {
     return {
-      msg: "Hola Vue!!!"
+      searchQuery: "",
+      tracks: []
     };
+  },
+
+  computed: {
+    searchMessage() {
+      return `Encontrados ${this.tracks.length}`;
+    }
+  },
+
+  methods: {
+    search() {
+      this.tracks = tracks;
+      console.log(this.searchQuery);
+    }
   }
 };
 </script>
 
 <style lang="scss">
 @import "./scss/main.scss";
+.results {
+  margin-top: 50px;
+}
 </style>
