2bc504aa2802fd26a468e100a5f3be3230d42b4a
georgerangelcode <georgerangelcode@gmail.com>
2019-07-02 12:15:48 -0500

    Creación de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template lang="pug">
   #app
+    pm-header
     section.section
-
       nav.nav.has-shadow
         .container
           input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
           a.button.is-info.is-large(v-on:click="search") Buscar
           a.button.is-danger.is-large &times;
-      
       .container
         p
           small {{searchMessage}}
-
       .container.results
         .columns
           .column(v-for="track in tracks") 
             | {{track.name}} - {{track.artists[0].name}}
+    pm-footer
 </template>
 
 <script>
 import trackSevice from "./services/trackService";
+import PmFooter from "./components/layout/Footer.vue";
+import PmHeader from "./components/layout/Header.vue";
 
 export default {
   name: "app",
+
+  components: {
+    PmFooter,
+    PmHeader
+  },
 
   data() {
     return {
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong Music
+          .nav-right.nav-menu
+    .hero-body
+      .container.has-text-centered
+        h1.title Music
+        h1.subtitle Canciones que estan Vue-nisimas!
+</template>
