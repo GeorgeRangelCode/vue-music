b503c20c10156f5575a345a804b92a5c97f8470e
georgerangelcode <georgerangelcode@gmail.com>
2019-07-08 17:19:55 -0500

    Instalar vue-router y configurar router view

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,21 @@
 <template lang="pug">
   #app
     pm-header
-    pm-notification(v-show="showNotification")
-      p(slot="body") No se encontraron resultados
-    pm-loader(v-show="isLoading")
-    section.section(v-show="!isLoading")
-      nav.nav
-        .container
-          input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
-          a.button.is-info.is-large(v-on:click="search") Buscar
-          a.button.is-danger.is-large &times;
-      .container
-        p
-          small {{searchMessage}}
-      .container.results
-        .columns.is-multiline
-          .column.is-one-quarter(v-for="track in tracks") 
-            pm-track(
-              v-bind:class="{ 'is-active': track.id == selectedTrack }"
-              v-bind:track="track" 
-              v-on:select="setSelectTrack"
-            )
+
+    router-view
+    
     pm-footer
 </template>
 
 <script>
-import trackSevice from "@/services/trackService";
 import PmFooter from "@/components/layout/Footer.vue";
 import PmHeader from "@/components/layout/Header.vue";
-import PmTrack from "@/components/track.vue";
-import PmLoader from "@/components/shared/Loader.vue";
-import PmNotification from "@/components/shared/Notification.vue";
 
 export default {
   name: "app",
-
   components: {
     PmFooter,
-    PmHeader,
-    PmTrack,
-    PmLoader,
-    PmNotification
-  },
-
-  data() {
-    return {
-      searchQuery: "",
-      tracks: [],
-      isLoading: false,
-      showNotification: false,
-      selectedTrack: ''
-    };
-  },
-
-  computed: {
-    searchMessage() {
-      return `Encontrados ${this.tracks.length}`;
-    }
-  },
-
-  watch: {
-    showNotification () {
-      if (this.showNotification) {
-        setTimeout(() => {
-          this.showNotification = false
-        }, 3000)
-      }
-    }
-  },
-
-  methods: {
-    search() {
-      if (!this.searchQuery) {
-        return;
-      }
-      this.isLoading = true;
-      trackSevice.search(this.searchQuery)
-        .then(response => {
-          this.showNotification = response.tracks.total == 0
-          this.tracks = response.tracks.items;
-          this.isLoading = false;
-        });
-    },
-
-    setSelectTrack (id) {
-      this.selectedTrack = id
-    }
+    PmHeader
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "@/App.vue";
+import routes from "@/routes";
 
 import eventBus from "@/plugins/event-bus"
 
+Vue.use(VueRouter)
 Vue.use(eventBus)
+
+const router = new VueRouter({ routes })
 
 new Vue({
   el: "#app",
-  render: h => h(App)
+  render: h => h(App),
+  router
 });
